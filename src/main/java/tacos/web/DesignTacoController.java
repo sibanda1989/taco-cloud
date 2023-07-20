a2e42b91365e21e13fa47fba50565370832b2ee0
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-07-20 22:13:39 +0200

    feat: use IngredientRepo to get data

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,6 +20,8 @@ import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
 import tacos.TacoOrder;
+import tacos.data.IngredientRepository;
+
 
 
 @Slf4j
@@ -27,20 +30,16 @@ import tacos.TacoOrder;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 	
+	private final IngredientRepository ingredientRepository;
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepository) {
+		this.ingredientRepository = ingredientRepository;
+	}
+	
 	@ModelAttribute
 	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "Cheddar", Type.CHEESE),
-				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "Salsa", Type.SAUCE),
-				new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-				);
+		Iterable<Ingredient> ingredients = ingredientRepository.findAll();
 		
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
@@ -77,7 +76,7 @@ public class DesignTacoController {
 	}
 	
 	private Iterable<Ingredient> filterByType(
-			List<Ingredient> ingredients, Type type){
+			Iterable<Ingredient> ingredients, Type type){
 		return ingredients.
 				stream().
 				filter(x -> x.getType().equals(type)).
