684ab28b96d4b8c0dcdefcb40ff1559253d3f310
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-07-24 21:36:12 +0200

    chore: arrange code into chapter folders

---
+++ ch03/jdbc/src/main/java/tacos/Taco.java
@@ -0,0 +1,21 @@
+package tacos;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Taco {
+	private Long id;
+	private Date createdAt;
+	@NotNull
+	@Size(min = 5, message = "Name should be at least 5 characters long")
+	private String name;
+	
+	@NotNull
+	@Size(min = 1, message = "You must add at least 1 ingredient")
+	private List<Ingredient> ingredients;
+}
