e556a8490960c5a28c85eacfb2119da400120ee1
Hastings Sibanda <hastingssbnd5@gmail.com>
2023-07-24 21:31:13 +0200

    feat: create order data logic

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,7 +1,7 @@
 package tacos;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +18,7 @@ public class TacoOrder implements Serializable{
 	
 	private Long id;
 	
-	private Date createdAt;
+	private Date placedAt;
 
 	@NotBlank(message = "Delivery Name is required")
 	private String deliveryName;
---
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -0,0 +1,84 @@
+package tacos.data;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.sql.Types;
+
+import org.springframework.asm.Type;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import tacos.Taco;
+import tacos.TacoOrder;
+
+public class JdbcOrderRepository implements OrderRepository {
+	private JdbcOperations jdbcOperations;
+
+	public JdbcOrderRepository(JdbcOperations jdbcOperations) {
+		this.jdbcOperations = jdbcOperations;
+	}
+
+
+
+	@Override
+	@Transactional
+	public TacoOrder save(TacoOrder tacoOrder) {
+		PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(
+				"insert into Taco_Order "
+						+ "(delivery_name, delivery_street, delivery_city, "
+						+ "delivery_state, delivery_zip, cc_number, "
+						+ "cc_expiration, cc_cvv, placed_at) "
+						+ "values (?,?,?,?,?,?,?,?,?)",
+						Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+						Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+						Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP
+				);
+		pscf.setReturnGeneratedKeys(true);
+		tacoOrder.setPlacedAt(new Date());
+		PreparedStatementCreator psc = pscf.newPreparedStatementCreator(
+				Arrays.asList(
+						tacoOrder.getDeliveryName(),
+						tacoOrder.getDeliveryStreet(),
+						tacoOrder.getDeliveryCity(),
+						tacoOrder.getDeliveryState(),
+						tacoOrder.getDeliveryZip(),
+						tacoOrder.getCcNumber(),
+						tacoOrder.getCcExpiration(),
+						tacoOrder.getCcCVV(),
+						tacoOrder.getPlacedAt()));
+		GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcOperations.update(psc, keyHolder);
+		long orderId = keyHolder.getKey().longValue();
+		tacoOrder.setId(orderId);
+		List<Taco> tacos = tacoOrder.getTacos();
+		int i = 0;
+		for (Taco taco : tacos) {
+			saveTaco(orderId, i++, taco);
+		}
+		return tacoOrder;
+	}
+
+
+
+	private long saveTaco(long orderId, int orderKey, Taco taco) {
+		taco.setCreatedAt(new Date());
+		PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(
+				"insert into Taco " + "(name, created_at, taco_order, taco_order_key) " + "values (?, ?, ?, ?)",
+				Types.VARCHAR, Types.TIMESTAMP, Type.LONG, Type.LONG);
+		pscf.setReturnGeneratedKeys(true);
+		PreparedStatementCreator psc = pscf
+				.newPreparedStatementCreator(Arrays.asList(taco.getName(), taco.getCreatedAt(), orderId, orderKey));
+		GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcOperations.update(psc, keyHolder);
+		long tacoId = keyHolder.getKey().longValue();
+		taco.setId(tacoId);
+		saveIngredientRefs(tacoId, taco.getIngredients());
+		return tacoId;
+		
+	}
+
+}
